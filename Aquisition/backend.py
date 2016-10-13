b5f68cedc185d8f2eb3e2d18dc1b7218ebf98e7d
zigorrom <zigorrom@gmail.com>
2016-10-13 15:57:26 +0200

    be

--- Aquisition/backend.py
+++ Aquisition/backend.py
@@ -1,4 +1,18 @@
+import subprocess, math
 
+import numpy as np
+from PyQt4 import QtCore
+
+class DAQthread(QtCore.QThread):
+    threadStarted = QtCore.pyqtSignal()
+    threadStopped = QtCore.pyqtSignal()
+    threadProgressChanged = QtCore.pyqtSignal()
+    threadTaskDone = QtCore.pyqtSignal()
+
+    def __init__(self,data_storage, parent = None):
+        super().__init__(parent)
+        self.data_storage = data_storage
+        
 
 
 def main():
