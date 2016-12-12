78d07a907faea497802c17143669625507e3afc7
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2016-12-12 19:26:34 +0100

    view models file

--- Aquisition/viewmodels.py
+++ Aquisition/viewmodels.py
@@ -2,7 +2,7 @@ from PyQt4 import QtCore, QtGui
 
 import sys
 
-class ChannelSettingsModel(QtCore.QAbstractTableModel):
+class MainViewModel():
     def __init__(self,parent = None):
         super().__init__(parent)
         
