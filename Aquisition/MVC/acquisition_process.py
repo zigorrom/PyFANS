bcb912ce2df471ee9a2e99787469bf7e9850043e
zigorrom <zigorrom@gmail.com>
2017-02-28 10:12:56 +0100

    123

--- Aquisition/MVC/acquisition_process.py
+++ Aquisition/MVC/acquisition_process.py
@@ -4,6 +4,8 @@ from PyQt4 import QtCore
 from multiprocessing import Process, Event, JoinableQueue
 from agilent_u2542a import *
 from scipy.signal import periodogram
+from scipy.signal import decimate
+import math
 
 """
 This class implements acquisition from hardware and fourier tranform
@@ -24,8 +26,132 @@ class Acquisition(Process):
     def stop(self):
         self.exit.set()
 
-    
+    def run(self):
+        sys.stdout = open("log.txt", "w")
+        data_queue = self.data_queue
         
+#        try:
+#            d = AgilentU2542A(self.visa_resource)
+#            d.daq_init_channels()
+#            counter = 0
+#            fs = self.sample_rate
+#            npoints = self.points_per_shot
+#            max_count = self.total_samples
+            
+#            d.daq_run()
+#            print("started")
+#            init_time = time.time()
+#            nchan = len(d.enabled_ai_channels)
+
+#            #functions for reducing dot anmount in cycle below
+#            need_exit = self.exit.is_set
+#            is_data_ready = d.daq_is_data_ready
+#            read_data = d.daq_read_data
+            
+#            f1_max = 3000
+#            new_fs = 10000
+#            decimation_factor = int( fs/new_fs)
+#            new_fs = int(fs/decimation_factor)
+#            total_array = np.zeros((nchan,fs))
+
+#            second_range = None
+#            first_range = None
+#            freq_2 = None
+#            freq_1 = None
+#            #f1_aver_counter = 0
+#            f2_aver_counter = 0
+#            fill_value = 0
+            
+               
+
+#            while (not need_exit()) and counter < max_count:
+#                try:
+#                    if is_data_ready():
+                        
+#                        f2_aver_counter += 1
+#                        new_fill_value = fill_value+npoints
+#                        data = read_data()
+#                        print("data have been read")
+                        
+#                        for i,idx in enumerate(range(fill_value, new_fill_value)):
+#                            for j in range(nchan):
+#                                total_array[j,idx] = data[j,i]
+#                                print("i,j,idx")
+#                                print(i)
+#                                print(j)
+#                                print(idx)
+                            
+                            
+                        
+#                        arr = total_array[:,fill_value:new_fill_value]
+#                        print("slice was taken")
+#                        print(arr)
+#                        fill_value = new_fill_value % fs
+
+#                        if second_range is None:
+#                            freq_2, second_range = periodogram(arr, fs)
+#                        else:
+#                            f, psd = periodogram(arr, fs)
+#                            #np.average((self.average, data['p']), axis=0, weights=(self.average_counter - 1, 1))
+#                            second_range = np.average((second_range,psd),axis=0,weights=(f2_aver_counter - 1, 1))   
+
+
+#                        if new_fill_value%fs == 0:
+#                            #fill_value = 0
+#                            decimated = decimate(total_array,decimation_factor,n=8,ftype="iir",axis = 1 ,zero_phase=True)
+#                            #print("decimated length = {0}".format(len(decimated)))
+#                            #print(decimated)
+#                            #f1_aver_counter += 1
+#                            #print(f1_aver_counter)
+#                            #if first_range is None:
+#                            freq_1, first_range = periodogram(decimated, new_fs)
+#                            #else:
+#                            #    f,psd = periodogram(decimated,new_fs)
+#                            #    first_range = np.average((first_range,psd),axis=0,weights=(f1_aver_counter - 1, 1)) 
+#                            #counter += npoints
+#                            t = time.time()-init_time
+
+#                            df1 = freq_1[1]
+#                            df2 = freq_2[1]
+
+#                            freq1_idx = math.floor(f1_max/df1)+1
+#                            freq2_idx = math.ceil(f1_max/df2)+1
+
+#                            res_freq  = np.hstack((freq_1[1:freq1_idx],freq_2[freq2_idx:]))
+#                            res = np.hstack((first_range[1:freq1_idx],second_range[freq2_idx:]))
+
+
+#                            #data = read_data()
+#                            print(t)
+#    ##                        print(len(data))
+#                            print(data)
+#                            freq, psd = periodogram(data,fs) 
+#    ##                        decimated_data = 
+#                            block = {
+#                                     "t": t,
+#                                     "d": np.copy(total_array),
+#                                     "f": res_freq , #np.delete(freq,1,0),
+#                                     "p": res  #np.delete(psd,1,1)
+#                                     }
+#                            data_queue.put(block)
+#                            print(psd)
+                        
+#                except Exception as e:
+#                    err = str(e)
+#                    print(err)
+#                    if err== 'overload':
+#                        counter = max_count
+#                        break
+                                    
+#        except Exception as e:
+#            print ("exception"+str(e))
+#        finally:
+#            d.daq_stop()
+###            data_queue.close()
+#            print("finished") 
+    
+
+        #Previous version
     def run(self):
         sys.stdout = open("log.txt", "w")
         data_queue = self.data_queue
