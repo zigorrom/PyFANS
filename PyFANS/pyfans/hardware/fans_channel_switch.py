4ba08511a02ab2a7376ebecbb683e05052082c54
zigorrom <zigorrom@gmail.com>
2018-08-15 18:24:07 +0200

    Fixed DUT change

--- PyFANS/pyfans/hardware/fans_channel_switch.py
+++ PyFANS/pyfans/hardware/fans_channel_switch.py
@@ -1,6 +1,12 @@
+import time
+from enum import Enum, IntEnum, unique
 import pyfans.hardware.modern_agilent_u2542a as daq
 import pyfans.hardware.modern_fans_controller as mfc
 
+@unique
+class SET_RESET_STATES(IntEnum):
+    ON = 0
+    OFF = 1
 
 class FANS_DUT_Switch:
     MAX_CHANNELS = 32
@@ -19,16 +25,29 @@ class FANS_DUT_Switch:
     def daq_device(self):
         return self.fans_controller.daq_parent_device
 
+    def switch_all_off(self):
+        for dut in range(self.MAX_CHANNELS):
+            self.switch_dut_state(dut, SET_RESET_STATES.OFF)
 
-    def switch_to_dut(self, dut):
+    # def switch_all_off(self):
+    #     for dut in range(self.MAX_CHANNELS):
+    #         self.switch_dut_state(dut, SET_RESET_STATES.OFF)
+
+    def switch_dut_state(self, dut, state):
         if not isinstance(dut, int):
             raise TypeError("Channel should be an integer from 0 to 31 inclusively")
 
         if not (0 <= dut < self.MAX_CHANNELS):
             raise ValueError("channel should be in the range from 0 to 31 inclusively")
 
-        self.daq_device.digital_write_bit(self.set_reset_bit, 1)
+        if not isinstance(state, SET_RESET_STATES):
+            raise TypeError("Wrong state type")
+
+        self.daq_device.digital_write_bit(self.set_reset_bit, state)
         self.daq_device.digital_write(self.control_channel, dut)
         self.daq_device.digital_pulse_bit(self.pulse_bit, pulse_width=0.005)
 
-        
+    def switch_to_dut(self, dut):
+        self.switch_all_off()
+        time.sleep(0.005)
+        self.switch_dut_state(dut, SET_RESET_STATES.ON)
